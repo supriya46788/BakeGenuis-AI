26e8cab03863a49e603982e3c916f05a81fc3406
Chaitanya <109964522+ChaitanyaBadukale777@users.noreply.github.com>
2025-08-23 13:03:10 +0000

    Update firebase

--- js/firebase.js
+++ js/firebase.js
@@ -2,16 +2,16 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.0/fireba
 import { getAuth, GoogleAuthProvider, signInWithPopup} from "https://www.gstatic.com/firebasejs/10.11.0/firebase-auth.js";
 
 
-// ✅ Firebase Config
+// Firebase Config
 const firebaseConfig = {
-    apiKey: "AIzaSyAOt1rdaHSqqyMgoHwoTwiot000ftgfASw",
-    authDomain: "bake-genius.firebaseapp.com",
-    projectId: "bake-genius",
-    storageBucket: "bake-genius.firebasestorage.app",
-    messagingSenderId: "789871840185",
-    appId: "1:789871840185:web:52cd3ff9e02ba9f1d78468",
-    measurementId: "G-Z9XMGQ6H25"
-  };
+  apiKey: "YOUR_API_KEY", // Replace with your actual API key
+  authDomain: "YOUR_AUTH_DOMAIN", // Replace with your actual auth domain
+  projectId: "YOUR_PROJECT_ID", // Replace with your actual project ID
+  storageBucket: "YOUR_STORAGE_BUCKET", // Replace with your actual storage bucket
+  messagingSenderId: "YOUR_MESSAGING_SENDER_ID", // Replace with your actual messaging sender ID
+  appId: "YOUR_APP_ID", // Replace with your actual app ID
+  measurementId: "YOUR_MEASUREMENT_ID" // Replace with your actual measurement ID if needed
+};
 
 
 // ✅ Initialize Firebase
