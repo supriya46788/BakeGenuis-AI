d610a56ccd77494327133c646d2caf0e8cfb55cf
UjjwalTomar0808 <ujjwaltomar.0808@gmail.com>
2025-09-22 19:18:16 +0530

    resolve merge conflicts

--- assets/js/about.js
+++ assets/js/about.js
@@ -116,28 +116,3 @@ document.addEventListener("DOMContentLoaded", function () {
     hamburger.classList.toggle("open");
   });
 });
-
-// ================= Dark Mode Toggle =================
-// const darkModeBtn = document.getElementById("darkModeToggle");
-// const darkModeIcon = darkModeBtn.querySelector("i");
-
-// // Apply saved preference on page load
-// if (localStorage.getItem("darkMode") === "enabled") {
-//   document.body.classList.add("dark-mode");
-//   darkModeIcon.classList.remove("fa-moon");
-//   darkModeIcon.classList.add("fa-sun");
-// }
-
-// darkModeBtn.addEventListener("click", () => {
-//   document.body.classList.toggle("dark-mode");
-
-//   if (document.body.classList.contains("dark-mode")) {
-//     localStorage.setItem("darkMode", "enabled");
-//     darkModeIcon.classList.remove("fa-moon");
-//     darkModeIcon.classList.add("fa-sun");
-//   } else {
-//     localStorage.setItem("darkMode", "disabled");
-//     darkModeIcon.classList.remove("fa-sun");
-//     darkModeIcon.classList.add("fa-moon");
-//   }
-// });
